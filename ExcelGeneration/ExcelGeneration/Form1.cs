6ca3c58a1f556b6ca4bec12918d4aee0a2c6aea6
Szuák Ákos <Akos.Szuak@bitclub.hu>
2021-10-07 01:35:44 +0200

    Létrehozás, megnyitás

--- ExcelGeneration/ExcelGeneration/Form1.cs
+++ ExcelGeneration/ExcelGeneration/Form1.cs
@@ -16,10 +16,35 @@ namespace ExcelGeneration
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
+
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
         public Form1()
         {
             InitializeComponent();            
-            LoadData();            
+            LoadData();
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = new Excel.Workbook();
+                xlSheet = new Excel.Worksheet();
+
+                //CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
         private void LoadData() {
             List<Flat> Flats = context.Flats.ToList();
