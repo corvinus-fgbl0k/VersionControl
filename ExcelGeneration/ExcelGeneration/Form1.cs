c750c7a5561af09ebccf02f0f4632e8ddff889d3
Szuák Ákos <Akos.Szuak@bitclub.hu>
2021-10-07 11:40:31 +0200

    Exportálás javítása

--- ExcelGeneration/ExcelGeneration/Form1.cs
+++ ExcelGeneration/ExcelGeneration/Form1.cs
@@ -76,21 +76,26 @@ namespace ExcelGeneration
             int counter = 0;
             foreach (Flat f in Flats)
             {
-                values[counter, 1] = f.Code;
-                values[counter, 2] = f.Vendor;
-                values[counter, 3] = f.Side;
-                values[counter, 4] = f.District;
-                values[counter, 5] = f.Elevator;
-                values[counter, 6] = f.NumberOfRooms;
-                values[counter, 7] = f.FloorArea;
-                values[counter, 8] = f.Price;
-                
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                values[counter, 4] = f.Elevator;
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "";
+
                 counter++;
             }
 
             xlSheet.get_Range(
              GetCell(2, 1),
              GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+            xlSheet.get_Range(
+                GetCell(2, 9),
+                GetCell(1 + values.GetLength(0),9)).Value2 = "=G2*H2";
 
             FormatTable();
         }
