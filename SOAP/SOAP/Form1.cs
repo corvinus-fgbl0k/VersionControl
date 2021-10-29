bb65a6e89b7cff28aa8784dbe5ed184bb4f34f4e
Szuák Ákos <Akos.Szuak@bitclub.hu>
2021-10-29 19:49:57 +0200

    Adatok megjelenítése diagramon

--- SOAP/SOAP/Form1.cs
+++ SOAP/SOAP/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace SOAP
@@ -22,8 +23,27 @@ namespace SOAP
             InitializeComponent();
             Lekerdezes();
             Loadxml(results);
+            DataChart();
             dataGridView1.DataSource = Rates;
-        }        
+        }
+
+        private void DataChart()
+        {
+            chartRateData.DataSource = Rates;
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+
+            series.BorderWidth = 2;
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+        }
+
         private void Lekerdezes()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
