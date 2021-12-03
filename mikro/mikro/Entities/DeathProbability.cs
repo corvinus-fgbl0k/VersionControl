b7a9938672c4515e099c1bf812359a39c9c389f2
Szuák Ákos <Akos.Szuak@bitclub.hu>
2021-12-03 22:55:02 +0100

    Osztályok létrehozása

---
+++ mikro/mikro/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mikro.Entities
+{
+    public class DeathProbability
+    {
+        public int Year { get; set; }
+        public Gender Gender { get; set; }
+        public double P { get; set; }
+    }
+}
